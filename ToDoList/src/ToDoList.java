587a8d64eaba2af549d056661a66cb3fa02a7035
Carlos Henrique Gomes <carlosgomes.2000@alunos.utfpr.edu.br>
2022-09-04 18:28:47 -0300

    Atualização do Código

--- ToDoList/src/ToDoList.java
+++ ToDoList/src/ToDoList.java
@@ -4,70 +4,51 @@ import java.util.Scanner;
 
 public class ToDoList {
 
-    public static Scanner scanner = new Scanner(System.in);
-
     private static List<String> Lista = new ArrayList<String>();
 
     public static void main(String[] args) {
-        int itemMenu = -1;
+        int resposta = -1;
+        int numero = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        while (itemMenu != 0) {
-            itemMenu = menu();
-            switch (itemMenu) {
+        while (resposta != 0) {
+
+            System.out.println("-------------\nLista");
+            for (String item : Lista) {
+                System.out.println(++numero + " - " + item);
+            }
+            System.out.println("-------------\n");
+
+            System.out.println("1 - Adicionar Item\n2 - Remover Item\n0 - Fechar\n");
+            System.out.print("Escolha uma opção: ");
+
+            resposta = scanner.nextInt();
+            switch (resposta) {
                 case 1:
-                    mostrarLista();
+                    Scanner scan = new Scanner(System.in); // Se não criar essa variável de scan no mesmo local que este
+                                                           // scan ele buga e não lê o teclado e acaba adicionando um
+                                                           // valor vazio a lista!!
+                    System.out.println("Adicionar Item a Lista\n");
+                    System.out.print("Nome do Item: ");
+
+                    String item = scan.nextLine();
+                    Lista.add(item);
                     break;
                 case 2:
-                    adicionarItem();
-                    break;
-                case 3:
-                    removerItem();
+                    System.out.println("\nRemover Item\n");
+                    System.out.print("Qual Item deseja remover?");
+                    int index = scanner.nextInt();
+                    if ((index - 1) < 0 || index > Lista.size()) {
+                        System.out.println("Item não encontrado!");
+                    } else {
+                        Lista.remove(index - 1);
+                    }
                     break;
                 case 0:
                     break;
                 default:
                     System.out.println("Escolha uma opção válida");
             }
-        }
-    }
-
-    public static int menu() {
-        System.out.println("\nMENU PRINCIPAL\n");
-        System.out.println("1 - Mostrar Lista\n2 - Adicionar Item\n3 - Remover Item\n0 - Fechar\n");
-        System.out.print("Escolha uma opção: ");
-        int resposta = scanner.nextInt();
-        return resposta;
-    }
-
-    public static void mostrarLista() {
-        System.out.println("-------------\nLista\n-------------");
-        int numero = 0;
-        for (String item : Lista) {
-            System.out.println(++numero + " - " + item);
-        }
-
-    }
-
-    public static void adicionarItem() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Adicionar Item a Lista\n");
-        System.out.print("Nome do Item: ");
-
-        String item = scanner.nextLine();
-        Lista.add(item);
-    }
-
-    public static void removerItem() {
-        mostrarLista();
-
-        System.out.println("\nRemover Item\n");
-        System.out.print("Qual Item deseja remover?");
-        int index = scanner.nextInt();
-        if ((index - 1) < 0 || index > Lista.size()) {
-            System.out.println("Item não encontrado! Selecione um Item de 1 a " + Lista.size());
-        } else {
-            Lista.remove(index - 1);
         }
     }
 }
